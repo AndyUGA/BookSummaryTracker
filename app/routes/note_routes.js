7b71efd038bc13c1a5e2b321553240399ab3303e
Andy <att35663@uga.edu>
2019-01-29 14:25:37 -0500

    Fixed issue with redirect when creating book summary

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -79,13 +79,8 @@ module.exports = function(app, db) {
 					
 
 				}
-					//oneLineContent.push(tempLine);
-					//console.log('78');
-					//console.log(oneLineContent);
 
 
-				console.log("oneLineContent");
-				console.log(oneLineContent);
 
 				//Render BookSummary.ejs from /Views/Andy/BookSummary
 				res.render('Andy/BookSummary', {BookInfo: item, oneLineContent: oneLineContent, id: id, lineAmount: lineAmount});
@@ -121,17 +116,15 @@ module.exports = function(app, db) {
 			if(err) {
 				res.send({'error': 'An error has occurred'});
 			} else {
-				res.redirect('/Andy/ListOfBooks');
+				res.redirect('/Andy/getListOfBooks');
 			}
 		});
 	});
 
 
 
-
-
 	//Display form to append to book summary
-	app.get('/getAppendForm/:id', (req, res) => {
+	app.get('/Andy/getAppendForm/:id', (req, res) => {
 		const id = req.params.id;
 		var collection = db.collection("AndyBookSummaries");
 
@@ -148,7 +141,7 @@ module.exports = function(app, db) {
 	
 
 	//Update book summary based on id
-	app.put('/appendContent/:id', (req, res) => {
+	app.put('/Andy/appendContent/:id', (req, res) => {
 		const id = req.params.id;
 		//const note = {content: res.content + " " +  req.body.content,title: res.title };
 
@@ -168,6 +161,164 @@ module.exports = function(app, db) {
 			} else {
 
 				res.redirect('/Andy/getBookSummary/' + id);
+				//res.redirect('/');
+			}
+		});
+	});
+
+
+
+
+
+
+
+
+
+
+	//Display list of books being read by user (ListOfBooks.ejs)
+	app.get('/Austin/getListOfBooks', (req, res) => {
+	
+		//Get information from mLab database 
+		var collection = db.collection("AustinBookSummaries");
+
+		//Returns all info in mLab database into JSON format
+		collection.find({}).toArray(function (err, result) {
+			if(err) {
+				res.send({ 'error': ' An error has occurred'});
+			} else {
+				//Render ListOfBooks.ejs in views directory
+				res.render('Austin/ListOfBooks', {result: result});
+			}
+		});
+	});
+
+	//Get Book summary information based on id
+	app.get('/Austin/getBookSummary/:id', (req, res) => {
+		//Get id from URL 
+		const id = req.params.id;
+
+		//Convert id from URL to object ID 
+		const details = {'_id': new ObjectID(id)};
+
+		//Find info about book summary based on the object id
+		db.collection('AustinBookSummaries').findOne(details, (err, item) => {
+			if(err) {
+				res.send({ 'error': ' An error has occurred'});
+			} else {
+				currentContent = item.content;
+				currentTitle = item.title;
+
+				var tempLine = "";
+				var oneLineContent = [];
+				var lineAmount = 0;
+
+				//currentContent = "abcde\nkdj\nfjkad";
+				console.log("currentContent.length is " + item.content.length);
+				console.log("currentContent is " + item.content);
+
+				for(var i = 0; i < currentContent.length; i++)
+				{
+					//console.log('i is ' + i);
+					console.log("Checking " + currentContent.substring(i,i+2));
+					if(currentContent.substring(i,i+2) != 'zz')
+					{
+						tempLine += currentContent.substring(i, i + 1);
+
+					}
+					else 
+					{
+						console.log("Pushing");
+						console.log(tempLine);
+						oneLineContent.push(tempLine);
+						lineAmount++;
+						tempLine = "";
+						i += 1;
+					}
+					
+
+				}
+
+
+
+				//Render BookSummary.ejs from /Views/Andy/BookSummary
+				res.render('Austin/BookSummary', {BookInfo: item, oneLineContent: oneLineContent, id: id, lineAmount: lineAmount});
+			}
+		});
+	});
+
+
+
+	//Display form to create book summary
+	app.get('/Austin/getBookForm', (req, res) => {
+		
+
+		var collection = db.collection("AustinBookSummaries");
+
+
+		collection.find({}).toArray(function (err, result) {
+			if(err) {
+				res.send({ 'error': ' An error has occurred'});
+			} else {
+
+				res.render('Austin/createBookSummary');
+			}
+		});
+	});
+
+
+	//Create book summary 
+	app.post('/Austin/createBookSummary', (req,res) => {
+
+		const note = {  title: req.body.title, content: req.body.content + "zz" };
+		db.collection('AustinBookSummaries').insert(note, (err, result) => {
+			if(err) {
+				res.send({'error': 'An error has occurred'});
+			} else {
+				res.redirect('/Austin/getListOfBooks');
+			}
+		});
+	});
+
+
+
+	//Display form to append to book summary
+	app.get('/Austin/getAppendForm/:id', (req, res) => {
+		const id = req.params.id;
+		var collection = db.collection("AustinBookSummaries");
+
+
+		collection.find({}).toArray(function (err, result) {
+			if(err) {
+				res.send({ 'error': ' An error has occurred'});
+			} else {
+
+				res.render('Ausitn/appendBookSummary', {BookInfo: result, id : id});
+			}
+		});
+	});
+	
+
+	//Update book summary based on id
+	app.put('/Austin/appendContent/:id', (req, res) => {
+		const id = req.params.id;
+		//const note = {content: res.content + " " +  req.body.content,title: res.title };
+
+
+
+
+
+
+		//Convert id from URL to object ID 
+		const details = {'_id': new ObjectID(id)};
+
+
+		const note = {  title: currentTitle , content: currentContent + req.body.content + "zz"};
+		db.collection('AustinBookSummaries').update(details, note, (err, item) => {
+			if(err) {
+				res.send({ 'error': ' An error has occurred'});
+			} else {
+
+				res.redirect('/Austin/getBookSummary/' + id);
 				//res.redirect('/');
 			}
 		});
